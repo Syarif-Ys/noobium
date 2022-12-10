ef400d7fc767f863628f34e1b989c4db25eeed6d
Syarif Ys <5yarif.y.s@gmail.com>
2022-12-10 21:43:49 +0800

    10 desember 2022

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import NavBar from '../components/NavBar'
-
+import type { NextPage } from "next";
+import Head from "next/head";
+import Article from "../components/Article";
+import NavBar from "../components/NavBar";
 
 const Home: NextPage = () => {
-  const articles = [... Array(4)].map((_, index) => {
+  const articles = [...Array(4)].map((_, index) => {
     return {
       id: index + 1,
-      title: "Learning redux Easier",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas etiam morbi varius sapien. Eu arcu morbi tortor rhoncus. Donec pellentesque diam orci enim, nibh diam. Nulla id ut risus quisque felis tristique metus..."
-    }
-  })
+      url: '/articles/how-to-learn-redux',
+      title: "Hpw to Learn Redux",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas etiam morbi varius sapien. Eu arcu morbi tortor rhoncus. Donec pellentesque diam orci enim, nibh diam. Nulla id ut risus quisque felis tristique metus...",
+      thumbnail: "/images/dummy-article-thumbnail.png",
+      category: "Technology",
+      date: "2022-09-20 16:00:00",
+      author: {
+        name: "John Doe",
+        photo: "/images/dummy-avatar.png",
+      },
+    };
+  });
   return (
     <div>
       <Head>
@@ -19,9 +28,22 @@ const Home: NextPage = () => {
       </Head>
 
       <NavBar />
-      <div className='w-[728px] wx-auto py-24'></div>
+      <div className="w-[720px] wx-auto py-24">
+        {articles.map((article) => (
+          <Article
+            key={article.id}
+            url={article.url}
+            title={article.title}
+            content={article.content}
+            thumbnail={article.thumbnail}
+            category={article.category}
+            date={article.date}
+            author={article.author}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
