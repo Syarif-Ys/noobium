4624c6717514341a0f0562166dedc528cd01dcff
Syarif Ys <5yarif.y.s@gmail.com>
2022-12-27 05:52:40 +0800

    ok

---
+++ pages/my-articles.tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Article from "../components/Article";
+import Category from "../components/Category";
+import NavBar from "../components/NavBar";
+
+const MyArticlesPage: NextPage = () => {
+  const router = useRouter()
+
+  const articles = [...Array(4)].map((_, index) => {
+    return {
+      id: index + 1,
+      slug: "how-to-learn-redux",
+      title: "How to Learn Redux",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas etiam morbi varius sapien. Eu arcu morbi tortor rhoncus. Donec pellentesque diam orci enim, nibh diam. Nulla id ut risus quisque felis tristique metus...",
+      thumbnail: "/images/dummy-article-thumbnail.png",
+      category: "Technology",
+      date: "2022-09-20 16:00:00",
+      author: {
+        name: "John Doe",
+        photo: "/images/dummy-avatar.png",
+      },
+    };
+  });
+  return (
+    <div>
+      <Head>
+        <title>Results for {router.query.keyword} | Noobium</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <NavBar />
+      <div className="w-[720px] mx-auto py-24">
+        <div className="mb-16 ">
+          <p className="font-sans font-bold text-slate-400 mb-3">Results For</p>
+          <p className="font-sans font-bold text-slate-900 text-5xl">
+            {router.query.keyword}
+          </p>
+        </div>
+        {articles.map((article) => (
+          <Article
+            key={article.id}
+            url={"/articles/${article.slug}"}
+            title={article.title}
+            content={article.content}
+            thumbnail={article.thumbnail}
+            category={article.category}
+            date={article.date}
+            author={article.author}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyArticlesPage;
