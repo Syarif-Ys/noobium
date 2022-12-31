1e0731b0d197e4c4477bc45b2bc81f1a179388ba
Syarif Ys <5yarif.y.s@gmail.com>
2022-12-31 11:02:55 +0800

    ok

--- pages/my-articles/create.tsx
+++ pages/my-articles/create.tsx
@@ -1,11 +1,31 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useRef } from "react";
 import NavBar from "../../components/NavBar";
 import ThumbnailPicker from "../../components/ThumbnailPicker";
+import Category from "../../components/Category";
 
 const CreateArticlePage: NextPage = () => {
+  const refContentInput = useRef<HTMLTextAreaElement>(null);
   const router = useRouter();
+
+  const categories = [...Array(10)].map((_, index) => {
+  return {
+    id: index + 1,
+    slug: 'technology',
+    name: 'Technology',
+  }
+})
+
+  const handleContentInputGrow = () => {
+    if (!refContentInput.current) return;
+
+    refContentInput.current.style.height = "auto";
+
+    refContentInput.current.style.height =
+      refContentInput.current.scrollHeight + "px";
+  };
 
   return (
     <div>
@@ -28,7 +48,20 @@ const CreateArticlePage: NextPage = () => {
           value=""
         />
         <ThumbnailPicker />
-        <textarea placeholder="Write an article here..." />
+        <textarea
+          ref={refContentInput}
+          className="w-full outline-none mt-12 font-serif text-slate-900"
+          placeholder="Write an article here..."
+          onInput={handleContentInputGrow}
+        />
+        <div className="pt-12 border-top border-slate-200">
+          <p className="font-sans text-slate-900 text-sm mb-4">Choose a Category</p>
+          <div className="flex flex-wrap gap-3">
+          {categories.map((category) => (
+            <Category key={category.id} label={category.name} />
+          ))}
+          </div>
+        </div>
       </div>
     </div>
   );
