a6adad14c12e734937fe9cafdf2410115d688dd7
Syarif Ys <5yarif.y.s@gmail.com>
2023-01-11 06:56:02 +0800

    ok

---
+++ pages/404.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Button from "../components/Button";
+import NavBar from "../components/NavBar";
+
+const NotFoundPage: NextPage = () => {
+  const categories = [...Array(10)].map((_, index) => {
+    return {
+      id: index + 1,
+      slug: "technology",
+      name: "Technology",
+    };
+  });
+
+  return (
+    <div>
+      <Head>
+        <title>404 | Noobium</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <NavBar hasSearchInput={false} />
+      <div
+        style={{ height: "calc(188vh - 4rem)" }}
+        className="w-[720px] flex flex-col items-center mx-auto py-24"
+      >
+        <h1 className="text-8xl text-slate-900 font-sans text-center font-bold mb-6">
+          404
+        </h1>
+        <p className="font-sans text-slate-900 text-center mb-14">
+          There&lsquo; no content here.
+        </p>
+        <Link href="/">
+          <Button type="button" size="large">
+            Back to Home
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
